f657a2dc3f80b5f78b13768cdcfad83d44cdd0df
afnan030503 <muhammadafnanmaulidi@gmail.com>
2024-12-18 13:11:17 +0700

    membuat object baik tabel dosen atau matakuliah

--- app/src/main/java/com/example/ucproomdatabase_204/ui/theme/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/ucproomdatabase_204/ui/theme/navigation/AlamatNavigasi.kt
@@ -6,3 +6,23 @@ interface AlamatNavigasi{
 object DestinasiHome : AlamatNavigasi{
     override val route = "Home"
 }
+object DestinasiDetailDosen : AlamatNavigasi{
+    override val route = "Detail"
+    const val NIDN = "nidn"
+    val routesWithArg = "$route/{$NIDN}"
+}
+object DestinasiUpdateDosen : AlamatNavigasi{
+    override val route = "update"
+    const val NIDN = "kode"
+    val routesWithArg = "$route/{$NIDN}"
+}
+object DestinasiDetailMatakuliah : AlamatNavigasi{
+    override val route = "Detail"
+    const val KODE = "kode"
+    val routesWithArg = "$route/{$KODE}"
+}
+object DestinasiUpdateMatakuliah : AlamatNavigasi{
+    override val route = "update"
+    const val KODE = "kode"
+    val routesWithArg = "$route/{$KODE}"
+}

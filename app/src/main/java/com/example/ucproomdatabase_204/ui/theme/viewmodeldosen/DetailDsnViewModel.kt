5b42a4ddc16316dbed785d75ad0293dfbf5a8de3
afnan030503 <muhammadafnanmaulidi@gmail.com>
2024-12-18 14:20:14 +0700

    membuat class DetailDsnViewModel

--- app/src/main/java/com/example/ucproomdatabase_204/ui/theme/viewmodeldosen/DetailDsnViewModel.kt
+++ app/src/main/java/com/example/ucproomdatabase_204/ui/theme/viewmodeldosen/DetailDsnViewModel.kt
@@ -1,6 +1,21 @@
 package com.example.ucproomdatabase_204.ui.theme.viewmodeldosen
 
+import android.view.View
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.ucproomdatabase_204.data.entity.Dosen
+import com.example.ucproomdatabase_204.repository.RepositoryDsn
+import com.example.ucproomdatabase_204.ui.theme.navigation.DestinasiDetailDosen
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 
 data class DetailUistate(
     val detailUiEvent: DosenEvent = Dosenevent(),
@@ -20,4 +35,41 @@ fun Dosen.toDetailUiEvent(): DosenEvent{
         nama = nama,
         jeniskelamin = jeniskelamin
     )
+}
+
+class DetailDsnViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryDsn: RepositoryDsn,
+): ViewModel(){
+    private val _nidn: String = checkNotNull(savedStateHandle[DestinasiDetailDosen.NIDN])
+
+    val detailUistate: StateFlow<DetailUistate> = repositoryDsn.getDsn(_nidn)
+        .filterNotNull()
+        .map {
+            DetailUistate(
+                detailUiEvent = it.toDetailUiEvent(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(DetailUistate(isLoading = true))
+            delay(600)
+        }
+        .catch {
+            emit(
+                DetailUistate(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "terjadi kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(2000),
+            initialValue = DetailUistate(
+                isLoading = true,
+            )
+        )
+
 }

4339b2b1f5d0e874b7c531fe9ed7e1b231b85c03
afnan030503 <muhammadafnanmaulidi@gmail.com>
2024-12-18 13:15:34 +0700

    membuat fun dan parameternya

--- app/src/main/java/com/example/ucproomdatabase_204/ui/theme/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/ucproomdatabase_204/ui/theme/navigation/PengelolaHalaman.kt
@@ -1,2 +1,13 @@
 package com.example.ucproomdatabase_204.ui.theme.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun PengelolaHalaman(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier = Modifier
+)
+{}

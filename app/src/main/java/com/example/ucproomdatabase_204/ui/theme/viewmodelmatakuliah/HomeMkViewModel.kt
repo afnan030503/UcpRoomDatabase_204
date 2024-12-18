2d7a124e20c1ee3ad8157615f5c631f3d3fa7f27
afnan030503 <muhammadafnanmaulidi@gmail.com>
2024-12-18 13:51:25 +0700

    membuat class HomeMkViewModel

--- app/src/main/java/com/example/ucproomdatabase_204/ui/theme/viewmodelmatakuliah/HomeMkViewModel.kt
+++ app/src/main/java/com/example/ucproomdatabase_204/ui/theme/viewmodelmatakuliah/HomeMkViewModel.kt
@@ -1,6 +1,13 @@
 package com.example.ucproomdatabase_204.ui.theme.viewmodelmatakuliah
 
 import com.example.ucproomdatabase_204.data.entity.Matakuliah
+import com.example.ucproomdatabase_204.repository.RepositoryDsn
+import com.example.ucproomdatabase_204.repository.RepositoryMk
+
+class HomeMkViewModel(
+    private val repositoryMk: RepositoryMk
+)
+
 
 data class HomeUiState(
     val listMk: List<Matakuliah> = listOf(),

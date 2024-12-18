3ce8ddfb5e43cda6a9e0bb1323d3b9d49fbd582f
afnan030503 <muhammadafnanmaulidi@gmail.com>
2024-12-18 14:12:20 +0700

    memindahkn data dari ui entity ke ui

--- app/src/main/java/com/example/ucproomdatabase_204/ui/theme/viewmodeldosen/DetailDsnViewModel.kt
+++ app/src/main/java/com/example/ucproomdatabase_204/ui/theme/viewmodeldosen/DetailDsnViewModel.kt
@@ -1,8 +1,23 @@
 package com.example.ucproomdatabase_204.ui.theme.viewmodeldosen
 
+import com.example.ucproomdatabase_204.data.entity.Dosen
+
 data class DetailUistate(
     val detailUiEvent: DosenEvent = Dosenevent(),
     val isLoading: Boolean= false,
     val isError: Boolean= false,
     val errorMessage: String = ""
-)
+){
+    val isUiEventEmpty: Boolean
+        get() = detailUiEvent == DosenEvent()
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != DosenEvent()
+}
+
+fun Dosen.toDetailUiEvent(): DosenEvent{
+    return DosenEvent(
+        nidn = nidn,
+        nama = nama,
+        jeniskelamin = jeniskelamin
+    )
+}

4651fab6fc023249141eec74af9d8931c3776b62
afnan030503 <muhammadafnanmaulidi@gmail.com>
2024-12-18 14:27:28 +0700

    membuat data class di DetailMkViewModel.kt

--- app/src/main/java/com/example/ucproomdatabase_204/ui/theme/viewmodeldosen/DetailDsnViewModel.kt
+++ app/src/main/java/com/example/ucproomdatabase_204/ui/theme/viewmodeldosen/DetailDsnViewModel.kt
@@ -16,6 +16,7 @@ import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
 data class DetailUistate(
     val detailUiEvent: DosenEvent = Dosenevent(),
@@ -71,5 +72,4 @@ class DetailDsnViewModel(
                 isLoading = true,
             )
         )
-
 }
--- app/src/main/java/com/example/ucproomdatabase_204/ui/theme/viewmodelmatakuliah/DetailMkViewModel.kt
+++ app/src/main/java/com/example/ucproomdatabase_204/ui/theme/viewmodelmatakuliah/DetailMkViewModel.kt
@@ -1,3 +1,16 @@
 package com.example.ucproomdatabase_204.ui.theme.viewmodelmatakuliah
 
+import com.example.ucproomdatabase_204.data.entity.Matakuliah
 
+
+data class DetailUiMState(
+    val detailUiEvent: MatakuliahEvent = MatakuliahEvent(),
+    val isLoading : Boolean = false,
+    val isError : Boolean = false,
+    val errorMessage: String = ""
+){
+    val isUiEventEmpty: Boolean
+        get()= detailUiEvent == MatakuliahEvent()
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != MatakuliahEvent()
+}

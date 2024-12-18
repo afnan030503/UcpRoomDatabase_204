4972466c077c561badeae3fc0a476cdefbd71c76
afnan030503 <muhammadafnanmaulidi@gmail.com>
2024-12-18 12:39:58 +0700

    implementasi crud

--- app/src/main/java/com/example/ucproomdatabase_204/repository/LocalRepositoryMk.kt
+++ app/src/main/java/com/example/ucproomdatabase_204/repository/LocalRepositoryMk.kt
@@ -1,8 +1,23 @@
 package com.example.ucproomdatabase_204.repository
 
 import com.example.ucproomdatabase_204.data.dao.MatakuliahDao
+import com.example.ucproomdatabase_204.data.entity.Matakuliah
+import kotlinx.coroutines.flow.Flow
 
 class LocalRepositoryMk(
     private val matakuliahDao: MatakuliahDao
-) : RepositoryMk{
+) : RepositoryMk {
+
+    override suspend fun insertMk(matakuliah: Matakuliah) {
+        matakuliahDao.insertMatakuliah(matakuliah)
+    }
+    override fun getAllMk(): Flow<List<Matakuliah>> {
+        return matakuliahDao.getAllMatakuliah()
+    }clas
+    override fun getMk(nidn: String): Flow<Matakuliah>{
+        return matakuliahDao.getMatakuliah(nidn)
+    }
+    override suspend fun updateMk(matakuliah: Matakuliah){
+        matakuliahDao.updateMatakuliah(matakuliah)
+    }
 }
--- app/src/main/java/com/example/ucproomdatabase_204/repository/RepositoryMk.kt
+++ app/src/main/java/com/example/ucproomdatabase_204/repository/RepositoryMk.kt
@@ -1,4 +1,6 @@
 package com.example.ucproomdatabase_204.repository
 
+import com.example.ucproomdatabase_204.data.entity.Matakuliah
+
 interface RepositoryMk {
 }

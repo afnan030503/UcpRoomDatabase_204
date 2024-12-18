6eebdcd33b3ac28b31d24bcf6d05a3b7e56cd723
afnan030503 <muhammadafnanmaulidi@gmail.com>
2024-12-18 12:42:18 +0700

    membuat crud di RepositoryMk

--- app/src/main/java/com/example/ucproomdatabase_204/repository/RepositoryMk.kt
+++ app/src/main/java/com/example/ucproomdatabase_204/repository/RepositoryMk.kt
@@ -1,6 +1,17 @@
 package com.example.ucproomdatabase_204.repository
 
 import com.example.ucproomdatabase_204.data.entity.Matakuliah
+import kotlinx.coroutines.flow.Flow
+
 
 interface RepositoryMk {
+    suspend fun insertMk(matakuliah: Matakuliah)
+
+    fun getAllMk() : Flow<List<Matakuliah>>
+
+    fun getMk(nidn: String) : Flow<Matakuliah>
+
+    suspend fun deleteMk(matakuliah: Matakuliah)
+
+    suspend fun updateMk(matakuliah: Matakuliah)
 }

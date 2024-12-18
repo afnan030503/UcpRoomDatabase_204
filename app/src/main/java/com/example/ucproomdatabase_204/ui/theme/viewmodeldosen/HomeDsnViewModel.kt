640f50379a7b4f284032ed83255fff48c7dec976
afnan030503 <muhammadafnanmaulidi@gmail.com>
2024-12-18 13:37:03 +0700

    membuat class HomeDsnViewModel dan parameternya

--- app/src/main/java/com/example/ucproomdatabase_204/ui/theme/viewmodeldosen/HomeDsnViewModel.kt
+++ app/src/main/java/com/example/ucproomdatabase_204/ui/theme/viewmodeldosen/HomeDsnViewModel.kt
@@ -1,7 +1,11 @@
 package com.example.ucproomdatabase_204.ui.theme.viewmodeldosen
 
 import com.example.ucproomdatabase_204.data.entity.Dosen
+import com.example.ucproomdatabase_204.repository.RepositoryDsn
 
+class HomeDsnViewModel(
+    private val repositoryDsn: RepositoryDsn
+)
 data class HomeUiState(
     val listDsn: List<Dosen> = listOf(),
     val isLoading: Boolean= false,

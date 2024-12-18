6799b7d1559fd8bd669b433046f79fe5e670bc4a
afnan030503 <muhammadafnanmaulidi@gmail.com>
2024-12-18 12:57:29 +0700

    membuat object DestinasiHome

--- app/src/main/java/com/example/ucproomdatabase_204/ui/theme/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/ucproomdatabase_204/ui/theme/navigation/AlamatNavigasi.kt
@@ -3,3 +3,6 @@ package com.example.ucproomdatabase_204.ui.theme.navigation
 interface AlamatNavigasi{
     val route: String
 }
+object DestinasiHome : AlamatNavigasi{
+    override val route = "Home"
+}

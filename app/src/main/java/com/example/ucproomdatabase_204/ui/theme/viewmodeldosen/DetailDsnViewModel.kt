880a9ef34a2ebf9a84b932d2877e589c2fa27bd8
afnan030503 <muhammadafnanmaulidi@gmail.com>
2024-12-18 14:05:57 +0700

    membuat data clas di DetailDsnViewModel.kt

--- app/src/main/java/com/example/ucproomdatabase_204/ui/theme/viewmodeldosen/DetailDsnViewModel.kt
+++ app/src/main/java/com/example/ucproomdatabase_204/ui/theme/viewmodeldosen/DetailDsnViewModel.kt
@@ -1,2 +1,8 @@
 package com.example.ucproomdatabase_204.ui.theme.viewmodeldosen
 
+data class DetailUistate(
+    val detailUiEvent: DosenEvent = Dosenevent(),
+    val isLoading: Boolean= false,
+    val isError: Boolean= false,
+    val errorMessage: String = ""
+)

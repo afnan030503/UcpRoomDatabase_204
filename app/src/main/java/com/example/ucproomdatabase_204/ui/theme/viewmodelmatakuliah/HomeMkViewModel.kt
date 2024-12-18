f06a9c9653acce7d072003e9f1d70c4964385aea
afnan030503 <muhammadafnanmaulidi@gmail.com>
2024-12-18 13:50:09 +0700

    membut data class viewmodelmk

--- app/src/main/java/com/example/ucproomdatabase_204/ui/theme/viewmodelmatakuliah/HomeMkViewModel.kt
+++ app/src/main/java/com/example/ucproomdatabase_204/ui/theme/viewmodelmatakuliah/HomeMkViewModel.kt
@@ -1,3 +1,10 @@
 package com.example.ucproomdatabase_204.ui.theme.viewmodelmatakuliah
 
+import com.example.ucproomdatabase_204.data.entity.Matakuliah
 
+data class HomeUiState(
+    val listMk: List<Matakuliah> = listOf(),
+    val isLoading: Boolean= false,
+    val isError: Boolean= false,
+    val errorMessage: String =""
+)

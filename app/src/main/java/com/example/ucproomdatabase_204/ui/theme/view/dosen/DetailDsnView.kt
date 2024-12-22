0ac090b3584d96630dbf6b75a8b0c5060188921e
afnan030503 <muhammadafnanmaulidi@gmail.com>
2024-12-23 02:41:44 +0700

    nenbuat tampilan dan hanya melihat tanpa delete

--- app/src/main/java/com/example/ucproomdatabase_204/ui/theme/view/dosen/DetailDsnView.kt
+++ app/src/main/java/com/example/ucproomdatabase_204/ui/theme/view/dosen/DetailDsnView.kt
@@ -1,8 +1,13 @@
 package com.example.ucproomdatabase_204.ui.theme.view.dosen
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucproomdatabase_204.ui.theme.costumwidget.cmtopBar
 import com.example.ucproomdatabase_204.ui.theme.viewmodeldosen.DetailDsnViewModel
 
 @Composable
@@ -11,5 +16,22 @@ fun DetailDsnView(
     viewModel: DetailDsnViewModel = viewModel(factory = PenyediaDsnViewModel.Factory),
     onBack: () -> Unit = {},
 ){
-    
+   Scaffold(
+       topBar = {
+           cmtopBar(
+               judul = " Detail Dosen",
+               showBackButton = true,
+               onBack = onBack,
+               modifier = Modifier
+           )
+       },
+           ) { innerPadding ->
+       val detailUistate by viewModel.detailUistate.collectAsState()
+
+       BodyDetailDsn(
+           modifier = Modifier.padding(innerPadding),
+           detailUistate = detailUistate,
+       )
+   }
+
 }

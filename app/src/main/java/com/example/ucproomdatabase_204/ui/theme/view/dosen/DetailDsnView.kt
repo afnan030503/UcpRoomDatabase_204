abe355f8b9f75ac3b2434c2357779590f9b6d209
afnan030503 <muhammadafnanmaulidi@gmail.com>
2024-12-23 02:49:33 +0700

    membuat reaksi jika data tidak ada

--- app/src/main/java/com/example/ucproomdatabase_204/ui/theme/view/dosen/DetailDsnView.kt
+++ app/src/main/java/com/example/ucproomdatabase_204/ui/theme/view/dosen/DetailDsnView.kt
@@ -1,14 +1,25 @@
 package com.example.ucproomdatabase_204.ui.theme.view.dosen
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.ucproomdatabase_204.ui.theme.costumwidget.cmtopBar
 import com.example.ucproomdatabase_204.ui.theme.viewmodeldosen.DetailDsnViewModel
+import com.example.ucproomdatabase_204.ui.theme.viewmodeldosen.DetailUistate
 
 @Composable
 fun DetailDsnView(
@@ -34,4 +45,34 @@ fun DetailDsnView(
        )
    }
 
+}
+
+@Composable
+fun BodyDetailDsn(
+    modifier: Modifier = Modifier,
+    detailUistate: DetailUistate = DetailUistate(),
+){
+    when{
+        detailUistate.isLoading -> {
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ){
+                CircularProgressIndicator()//tampil loading
+            }
+        }
+        detailUistate.isUiEventNotEmpty ->{
+            Column(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                ItemDetailDsn(
+                    dosen = detailUistate.detailUiEvent.toDosenEntity(),
+                    modifier = Modifier
+                )
+            }
+        }
+
+    }
 }

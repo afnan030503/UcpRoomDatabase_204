d014e6af967da9b4dfc4bda401034ae160cf7889
afnan030503 <muhammadafnanmaulidi@gmail.com>
2024-12-24 02:34:19 +0700

    membuat class DsnUIState

--- app/src/main/java/com/example/ucproomdatabase_204/ui/theme/view/dosen/InsertDsnView.kt
+++ app/src/main/java/com/example/ucproomdatabase_204/ui/theme/view/dosen/InsertDsnView.kt
@@ -1,6 +1,192 @@
 package com.example.ucproomdatabase_204.ui.theme.view.dosen
 
+import android.view.View
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucproomdatabase_204.ui.theme.costumwidget.cmtopBar
+import com.example.ucproomdatabase_204.ui.theme.navigation.AlamatNavigasi
+import com.example.ucproomdatabase_204.ui.theme.viewmodeldosen.PenyediaDsnViewModel
+import kotlinx.coroutines.launch
 
 @Composable
-fun InsertBodyDsn()
+fun InsertBodyDsn(
+    modifier: Modifier = Modifier,
+    onValueChange: (DosenEvent) -> Unit,
+    uiState: DsnUIState,
+    onClick: () -> Unit
+){
+    Column(modifier = modifier.fillMaxWidth()),
+    verticalArrangment = Arrangement.Center,
+    horizontalAligment = Alignment.CenterHorizontally
+    {
+        FormDosen(
+            dosenEvent = uiState.dosenEvent,
+            onValueChange = onValueChange,
+            errorState = uiState.isEntryValid,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onClick,
+            modifier = Modifier.fillMaxWidth(),
+        ) {
+            Text("Simpan")
+        }
+    }
+}
+object DestinasiInsert : AlamatNavigasi{
+    override val route: String = "insert_dsn"
+}
+@Composable
+fun InsertDsnView(
+    onBack: ()-> Unit,
+    onNavigate: ()-> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: DosenViewModel = viewModel(factory = PenyediaDsnViewModel.Factory)
+){
+    val uiState = viewModel.uiState
+    val snackbarHostState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
+
+    LaunchedEffect(uiState.snackBarMessage) {
+        uiState.snackBarMessage?.let { message->
+            coroutineScope.launch {
+                snackbarHostState.showSnackbar(message) // tampilkan snackbar
+                viewModel.resetSnackBarMessage()
+            }
+        }
+    }
+
+    Scaffold(
+        modifier = Modifier,
+        snackbarHost = {
+            SnackbarHost(
+                hostState = snackbarHostState
+            )
+        }
+    ) { padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .padding(16.dp)
+        ) {
+
+            cmtopBar(
+                onBack = onBack,
+                showBackButton = true,
+                judul = "Tambah Dosen"
+            )
+            //isi body
+            InsertBodyDsn(
+                uiState = uiState,
+                onValueChange = {updateEvent ->
+                    viewModel.updateState(updateEvent) //update state di view model
+                },
+                onClick = {
+                    coroutineScope.launch {
+                        viewModel.saveData()//simpan data
+                    }
+                    onNavigate()
+                }
+            )
+        }
+    }
+}
+
+
+@Composable
+fun FormDosen(
+    dosenEvent: DosenEvent = DosenEvent(),
+    onValueChange: (DosenEvent) -> Unit,
+    errorState: FormErrorState = FormErrorState(),
+    modifier: Modifier = Modifier
+) {
+    val jeniskelamin = listOf("Laki-Laki", "Perempuan")
+
+
+    Column(
+        modifier = modifier.fillMaxWidth()
+    ) {
+
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = DosenEvent.nama,
+            onValueChange = {
+                onValueChange(DosenEvent.copy(nama = it))
+            },
+            label = { Text("Nama") },
+            isError = errorState.nama != null,
+            placeholder = { Text("Masukkan Nama") },
+        )
+        Text(
+            text = errorState.nama ?: "",
+            color = Color.Red
+        )
+
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = dosenEvent.nidn, onValueChange = {
+                onValueChange(dosenEvent.copy(nidn = it))
+            },
+            label = { Text("NIDN") },
+            isError = errorState.nidn != null,
+            placeholder = { Text("Masukkan Nidn") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
+        Text(text = errorState.nidn ?: "", color = Color.Red)
+
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = "Jenis Kelamin")
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            jeniskelamin.forEach { jk ->
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Start
+                ) {
+                    RadioButton(
+                        selected = dosenEvent.jeniskelamin == jk,
+                        onClick = {
+                            onValueChange(dosenEvent.copy(jeniskelamin = jk))
+                        },
+                    )
+                    Text(
+                        text = jk,
+
+                        )
+                }
+            }
+        }
+        Text(
+            text = errorState.jeniskelamin ?: "",
+            color = Color.Red
+        )
+
+    }
+}
+
--- app/src/main/java/com/example/ucproomdatabase_204/ui/theme/viewmodeldosen/DosenViewModel.kt
+++ app/src/main/java/com/example/ucproomdatabase_204/ui/theme/viewmodeldosen/DosenViewModel.kt
@@ -1,2 +1,7 @@
 package com.example.ucproomdatabase_204.ui.theme.viewmodeldosen
 
+data class DsnUIState(
+    val dosenEvent: DosenEvent = DosenEvent(),
+    val isEntryValid: FormErrorState = FormErrorState(),
+    val snacBarMessage: String? = null,
+)
